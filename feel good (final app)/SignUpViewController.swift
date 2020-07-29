812ff699251e5ae3dbbeaf99ec5b98773cffabae
Leah Kvares <leahaviv@icloud.com>
2020-07-29 10:05:17 -0400

    Signup page

---
+++ feel good (final app)/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  feel good (final app)
+//
+//  Created by Leah on 7/28/20.
+//  Copyright © 2020 Leah Kvares. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
