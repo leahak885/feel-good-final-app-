2f935b14ac19c97d46d9f3acadb618ce07f08c8e
Leah Kvares <leahaviv@icloud.com>
2020-07-30 01:40:20 -0400

    addons2

---
+++ feel good (final app)/RecipesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipesViewController.swift
+//  feel good (final app)
+//
+//  Created by Leah on 7/30/20.
+//  Copyright © 2020 Leah Kvares. All rights reserved.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
